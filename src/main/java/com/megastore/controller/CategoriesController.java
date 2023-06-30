7aca8670af95551b3c570cf15c2fc1f7e7a8a3da
Vita <vita_victorivna@ukr.net>
2023-06-30 22:28:50 +0300

    refactor second

---
+++ src/main/java/com/megastore/controller/CategoriesController.java
@@ -0,0 +1,9 @@
+package com.megastore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categories")
+public class CategoriesController {
+}
---
+++ src/main/java/com/megastore/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.megastore.controller;
+
+import com.megastore.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/product")
+public class ProductController {
+
+    @GetMapping("/hot")
+    public List<Product> getHotProducts(List<Product> productList) {
+        return productList.stream()
+                .filter(hotProducts -> hotProducts.getIsHotProduct().equals(true))
+                .limit(9)
+                .collect(Collectors.toList());
+    }
+}
