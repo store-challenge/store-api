c1e8f169b646b4f80e9aaed904a233c0eaae3829
FedorenkoTaras <tarakhanio84@gmail.com>
2023-07-28 15:42:30 +0300

    create hot products endpoint

--- src/main/java/com/megastore/controller/ProductController.java
+++ src/main/java/com/megastore/controller/ProductController.java
@@ -1,32 +1,36 @@
 package com.megastore.controller;
 
+import com.megastore.data.dto.ProductHotDto;
+import com.megastore.data.dto.ProductPLPDto;
+import com.megastore.facade.ProductFacade;
 import com.megastore.model.Product;
-import com.megastore.service.impl.ProductServiceImpl;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/products")
-@RequiredArgsConstructor
+@RequestMapping("/api/v1/product")
+
 public class ProductController {
 
-    private final ProductServiceImpl productServiceImpl;
+    private final ProductFacade productFacade;
 
-//    @GetMapping("/hot")
-//    public Optional<Product> getHotProducts(Integer countProducts) {
-//
-//        Optional<Product> searchedProducts = productServiceImpl.findHotProducts();
-//        searchedProducts.stream().filter(hotProducts -> hotProducts.getIsHotProduct().equals(true))
-//                .limit(countProducts)
-//                .collect(Collectors.toList());
-//        return searchedProducts;
-//    }
+    public ProductController(ProductFacade productFacade) {
+        this.productFacade = productFacade;
+    }
+
+    @GetMapping("/hot")
+    public ResponseEntity<List<ProductHotDto>> getHotProducts(@RequestParam(defaultValue = "4") int limit, @RequestParam(defaultValue = "1") long catId) {
+        return ResponseEntity.ok(new ArrayList<>(productFacade.findHotProducts(limit,catId)));
+
+    }
 //    @GetMapping("/list")
 //    public List<Product> showProducts() {
 //        List<Product> searchedProducts = productServiceImpl.findAll();
--- src/main/java/com/megastore/facade/impl/ProductFacadeImpl.java
+++ src/main/java/com/megastore/facade/impl/ProductFacadeImpl.java
@@ -1,12 +1,25 @@
 package com.megastore.facade.impl;
 
+import com.megastore.data.dto.ProductHotDto;
 import com.megastore.data.dto.ProductPDPDto;
 import com.megastore.data.dto.ProductPLPDto;
+import com.megastore.data.dto.SubCategoriesDto;
 import com.megastore.facade.ProductFacade;
+import com.megastore.service.ProductService;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
 
+@Service
 public class ProductFacadeImpl implements ProductFacade {
+
+    private final ProductService productService;
+
+    public ProductFacadeImpl(ProductService productService) {
+        this.productService = productService;
+    }
+
     @Override
     public ProductPDPDto findById(Long id) {
         return null;
@@ -15,5 +28,10 @@ public class ProductFacadeImpl implements ProductFacade {
     @Override
     public Collection<ProductPLPDto> findAll() {
         return null;
+    }
+
+    @Override
+    public List<ProductHotDto> findHotProducts(int limit, long catId) {
+        return productService.findHotProducts(limit, catId).stream().map(ProductHotDto::new).toList();
     }
 }
