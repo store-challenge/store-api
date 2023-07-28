5d871be14e0cf748cd8c3d36a01d143c8b614e90
FedorenkoTaras <tarakhanio84@gmail.com>
2023-07-28 15:04:49 +0300

    create get subcategory endpoint

--- src/main/java/com/megastore/repository/ProductRepository.java
+++ src/main/java/com/megastore/repository/ProductRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface ProductRepository extends BaseRepository<Product> {
-    @Query("SELECT p FROM Product p WHERE p.isHotProduct = true")
-    Optional<Product> findHotProducts();
+   @Query(value = "SELECT * FROM product p JOIN subcategories s ON p.subcategory_id = s.id JOIN categories c ON s.category_id = c.id WHERE p.product_is_hot = true and s.category_id =:catId ORDER BY RANDOM()LIMIT :limit  ",nativeQuery = true)
+    List<Product> findTopByIsHotProduct(int limit, long catId);
 }
