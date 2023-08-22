ce86d38a0f6cd039715a295a934cdcc4af6878c1
Vita <vita_victorivna@ukr.net>
2023-08-22 21:11:24 +0300

    refactored filter by brand

--- src/main/java/com/megastore/repository/ProductRepository.java
+++ src/main/java/com/megastore/repository/ProductRepository.java
@@ -15,14 +15,6 @@ public interface ProductRepository extends BaseRepository<Product> {
             countQuery = "SELECT COUNT(p.id) FROM product p JOIN subcategories s ON p.subcategory_id = s.id JOIN categories c ON s.category_id = c.id JOIN images i ON p.id = i.product_id WHERE p.product_hot = true AND s.category_id = :catId",
             nativeQuery = true)
     List<Product> findTopByIsHotProduct(Pageable pageable, @Param("catId") long catId);
-    @Query(value = "SELECT * " +
-            "FROM product p " +
-            "JOIN subcategories s ON p.subcategory_id = s.id " +
-            "JOIN categories c ON s.category_id = c.id " +
-            "WHERE p.product_hot = true and s.category_id =:catId " +
-            "ORDER BY RANDOM() " +
-            "LIMIT :limit  ", nativeQuery = true)
-    List<Product> findTopByIsHotProduct(int limit, long catId);
 
     @Query(value = "SELECT * " +
             "FROM product p " +
@@ -31,11 +23,11 @@ public interface ProductRepository extends BaseRepository<Product> {
             "JOIN images img ON img.product_id = p.id " +
             "WHERE s.id = :subcategoryId " +
             "AND p.product_price BETWEEN :priceFrom AND :priceTo " +
-            "AND p.product_brand = :brand " +
+            "AND (COALESCE(:brand, '') = '' OR p.product_brand LIKE :brand) " +
             "ORDER BY p.updated DESC " +
             "LIMIT :limit ",
             nativeQuery = true)
-    Collection<Product> findAll(int limit, long subcategoryId, Double priceFrom, Double priceTo, String brand);
+    Collection<Product> findAll(@Param("limit") int limit, @Param("subcategoryId") long subcategoryId, @Param("priceFrom") Double priceFrom, @Param("priceTo") Double priceTo, @Param("brand") String brand);
 
     @Query(value = "SELECT * " +
             "FROM product p " +
@@ -44,11 +36,11 @@ public interface ProductRepository extends BaseRepository<Product> {
             "JOIN images img ON img.product_id = p.id " +
             "WHERE s.id = :subcategoryId " +
             "AND p.product_price BETWEEN :priceFrom AND :priceTo " +
-            "AND p.product_brand = :brand " +
+            "AND (COALESCE(:brand, '') = '' OR p.product_brand LIKE :brand) " +
             "ORDER BY p.product_title DESC, p.updated DESC " +
             "LIMIT :limit ",
             nativeQuery = true)
-    Collection<Product> findAllSortedByNameDESC(int limit, long subcategoryId, Double priceFrom, Double priceTo, String brand);
+    Collection<Product> findAllSortedByNameDESC(@Param("limit") int limit, @Param("subcategoryId") long subcategoryId, @Param("priceFrom") Double priceFrom, @Param("priceTo") Double priceTo, @Param("brand") String brand);
 
     @Query(value = "SELECT * " +
             "FROM product p " +
@@ -57,11 +49,11 @@ public interface ProductRepository extends BaseRepository<Product> {
             "JOIN images img ON img.product_id = p.id " +
             "WHERE s.id = :subcategoryId " +
             "AND p.product_price BETWEEN :priceFrom AND :priceTo " +
-            "AND p.product_brand = :brand " +
+            "AND (COALESCE(:brand, '') = '' OR p.product_brand LIKE :brand) " +
             "ORDER BY p.product_title ASC, p.updated DESC " +
             "LIMIT :limit ",
             nativeQuery = true)
-    Collection<Product> findAllSortedByNameASC(int limit, long subcategoryId, Double priceFrom, Double priceTo, String brand);
+    Collection<Product> findAllSortedByNameASC(@Param("limit") int limit, @Param("subcategoryId") long subcategoryId, @Param("priceFrom") Double priceFrom, @Param("priceTo") Double priceTo, @Param("brand") String brand);
 
     @Query(value = "SELECT * " +
             "FROM product p " +
@@ -70,11 +62,11 @@ public interface ProductRepository extends BaseRepository<Product> {
             "JOIN images img ON img.product_id = p.id " +
             "WHERE s.id = :subcategoryId " +
             "AND p.product_price BETWEEN :priceFrom AND :priceTo " +
-            "AND p.product_brand = :brand " +
+            "AND (COALESCE(:brand, '') = '' OR p.product_brand LIKE :brand) " +
             "ORDER BY p.product_price DESC, p.updated DESC " +
             "LIMIT :limit ",
             nativeQuery = true)
-    Collection<Product> findAllSortedByPriceDESC(int limit, long subcategoryId, Double priceFrom, Double priceTo, String brand);
+    Collection<Product> findAllSortedByPriceDESC(@Param("limit") int limit, @Param("subcategoryId") long subcategoryId, @Param("priceFrom") Double priceFrom, @Param("priceTo") Double priceTo, @Param("brand") String brand);
 
     @Query(value = "SELECT * " +
             "FROM product p " +
@@ -83,10 +75,10 @@ public interface ProductRepository extends BaseRepository<Product> {
             "JOIN images img ON img.product_id = p.id " +
             "WHERE s.id = :subcategoryId " +
             "AND p.product_price BETWEEN :priceFrom AND :priceTo " +
-            "AND p.product_brand = :brand " +
+            "AND (COALESCE(:brand, '') = '' OR p.product_brand LIKE :brand) " +
             "ORDER BY p.product_price ASC, p.updated DESC " +
             "LIMIT :limit ",
             nativeQuery = true)
-    Collection<Product> findAllSortedByPriceASC(int limit, long subcategoryId, Double priceFrom, Double priceTo, String brand);
+    Collection<Product> findAllSortedByPriceASC(@Param("limit") int limit, @Param("subcategoryId") long subcategoryId, @Param("priceFrom") Double priceFrom, @Param("priceTo") Double priceTo, @Param("brand") String brand);
 
 }
