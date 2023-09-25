ce278de5fd29d036b714d4ded62d9d6860c66012
Vita <vita_victorivna@ukr.net>
2023-09-25 18:06:05 +0300

    add fields brandId, brandName, categoryId, categoryName to product/list and ProductPLPDto.java

--- src/main/java/com/megastore/data/dto/ProductPLPDto.java
+++ src/main/java/com/megastore/data/dto/ProductPLPDto.java
@@ -1,5 +1,6 @@
 package com.megastore.data.dto;
 
+import com.megastore.model.Brand;
 import com.megastore.model.Product;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,24 +18,31 @@ public class ProductPLPDto {
     private Long id;
     private Date updated;
     private String title;
-    private Long brand;
+    private Long brandId;
+    private String brandName;
     private BigDecimal price;
     private boolean isHot;
     private Integer productAvailable;
     private Integer productArticle;
+    private Long categoryId;
+    private String categoryName;
     private Long subcategoryId;
+    private String subcategoryName;
 
     public ProductPLPDto(Product product) {
         this.id = product.getId();
         this.updated = product.getUpdated();
         this.title = product.getName();
-        this.brand = product.getBrand().getId();
+        this.brandId = product.getBrand().getId();
+        this.brandName = product.getBrand().getName();
         this.price = product.getPrice();
         this.isHot = product.getIsHotProduct();
         this.productAvailable = product.getProductAvailable();
         this.productArticle = product.getProductArticle();
         this.subcategoryId = product.getSubCategories().getId();
-
+        this.subcategoryName = product.getSubCategories().getName();
+        this.categoryId = product.getSubCategories().getCategories().getId();
+        this.categoryName = product.getSubCategories().getCategories().getName();
     }
 }
 
--- src/main/java/com/megastore/service/impl/ProductServiceImpl.java
+++ src/main/java/com/megastore/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.megastore.service.impl;
 
 import com.megastore.model.Brand;
+import com.megastore.model.Categories;
 import com.megastore.model.Product;
 import com.megastore.model.Images;
 import com.megastore.model.SubCategories;
@@ -97,7 +98,7 @@ public class ProductServiceImpl implements ProductService {
                                        Integer limit) {
 
         StringBuilder sql = new StringBuilder();
-        sql.append("SELECT * FROM product p ");
+        sql.append("SELECT *, c.name as category_name, b.name as brand_name FROM product p ");
         sql.append("JOIN subcategories s ON p.subcategory_id = s.id ");
         sql.append("JOIN categories c ON s.category_id = c.id ");
         sql.append("JOIN images img ON p.id = img.product_id ");
@@ -152,8 +153,16 @@ public class ProductServiceImpl implements ProductService {
             product.setProductAvailable(rs.getInt("product_available"));
             product.setProductArticle(rs.getInt("product_article"));
 
+
             SubCategories subCategories = new SubCategories();
             subCategories.setId(rs.getLong("subcategory_id"));
+            subCategories.setName(rs.getString("name"));
+            product.setSubCategories(subCategories);
+
+            Categories categories = new Categories();
+            categories.setId(rs.getLong("category_id"));
+            categories.setName(rs.getString("category_name"));
+            subCategories.setCategories(categories);
             product.setSubCategories(subCategories);
 
             List<Images> imagesList = new ArrayList<>();
@@ -165,6 +174,7 @@ public class ProductServiceImpl implements ProductService {
 
             Brand brandProduct = new Brand();
             brandProduct.setId(rs.getLong("brand_id"));
+            brandProduct.setName(rs.getString("brand_name"));
             product.setBrand(brandProduct);
 
             return product;
