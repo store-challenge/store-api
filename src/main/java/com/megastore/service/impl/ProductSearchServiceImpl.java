15cce146e12c73851f873fae9c6c14d76cb70ebd
FedorenkoTaras <tarakhanio84@gmail.com>
2023-10-12 23:43:48 +0300

    bug fix search endpoint

--- src/main/java/com/megastore/service/impl/ProductSearchServiceImpl.java
+++ src/main/java/com/megastore/service/impl/ProductSearchServiceImpl.java
@@ -5,7 +5,8 @@ import com.megastore.repository.ProductRepository;
 import com.megastore.service.ProductSearchService;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.*;
+
 @Service
 public class ProductSearchServiceImpl implements ProductSearchService {
     private final ProductRepository productRepository;
@@ -15,7 +16,34 @@ public class ProductSearchServiceImpl implements ProductSearchService {
     }
 
     @Override
-    public Collection<Product> search(String query) {
-        return productRepository.findAllByNameContainingIgnoreCase(query);
+    public List<Product> search(String query) {
+        query = query.trim();
+        if (query.isEmpty()) {
+            return Collections.emptyList();
+        }
+        List<Product> exactMatches = findExactMatches(query);
+        List<Product> partialMatches = findPartialMatches(query);
+
+        if (!exactMatches.isEmpty()) {
+            partialMatches.removeAll(exactMatches);
+            exactMatches.addAll(partialMatches);
+            return exactMatches;
+        }
+
+        return partialMatches;
+    }
+
+    private List<Product> findExactMatches(String query) {
+        return (List<Product>) productRepository.findExactMatch(query);
+    }
+
+    private List<Product> findPartialMatches(String query) {
+        String[] words = query.split("\\s+");
+        List<Product> partialMatches = new ArrayList<>();
+        for (String word : words) {
+            partialMatches.addAll(productRepository.findAllByNameContainingIgnoreCaseWithMultipleWords(word));
+        }
+        partialMatches.sort(Comparator.comparing(Product::getName));
+        return partialMatches;
     }
 }
