963ceae5ae778caa8552ed21333f958f0f2d28e2
FedorenkoTaras <tarakhanio84@gmail.com>
2023-10-14 00:40:25 +0300

    change field in table users

--- src/main/java/com/megastore/model/User.java
+++ src/main/java/com/megastore/model/User.java
@@ -14,8 +14,11 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column()
-    private String username;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     @Column(name = "phone_number")
     private String phoneNumber;
